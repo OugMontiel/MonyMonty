463d26efad0fb405d092ac6dbd2a3cdd41088037
PcGamer <gogoedy@hotmail.com>
2025-03-13 12:20:59 -0500

    feat: :art: Inicio de seccion
    Agregamos el boton, de incio de seccion, y creamos un pequeño stilo para el var

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">dashboard</router-link>
-      <router-link to="/NewMov">New Mov</router-link>
+      <div>
+        <h1>Mony Monty</h1> <!-- Icono de la aplicación -->
+      </div>
+      <div>
+        <router-link to="/">dashboard</router-link>
+        <router-link to="/NewMov">New Mov</router-link>
+      </div>
+      <div>
+        <!-- Envolver el icono en un router-link -->
+        <a href="/auth/google">
+          <i class="fi fi-brands-google" alt="Google"></i>
+        </a>
+      </div>
+
       <!-- Agrega más enlaces de navegación según tus rutas -->
     </nav>
     <router-view></router-view>
@@ -11,7 +23,7 @@
 
 <script>
 export default {
-  name: 'App'
+  name: "App",
 };
 </script>
 
@@ -19,6 +31,29 @@ export default {
 /* Estilos para tu aplicación */
 nav {
   display: flex;
-  gap: 10px;
+  justify-content: space-between;
+  align-items: center; /* Alinear verticalmente los elementos */
+  background-color:var(--color-fondo-nav); /* Color de fondo de la barra de navegación */
+  padding: .3em .4em; /* Espaciado interno */
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2); /* Sombra para dar profundidad */
 }
+
+nav div {
+  display: flex;
+  align-items: center; /* Alinear verticalmente los elementos dentro del div */
+}
+
+nav a, nav router-link {
+  color: var(--texto-primario); /* Color del texto de los enlaces */
+  text-decoration: none; /* Eliminar subrayado de los enlaces */
+  padding: 0.5em 0.3em ; /* Espaciado alrededor del texto */
+  border-radius: 5px; /* Bordes redondeados */
+  transition: background-color 0.3s, color 0.3s; /* Transiciones suaves */
+}
+
+nav a:hover, nav router-link:hover {
+  background-color: var(--color-fondo-hover); /* Color de fondo al pasar el mouse */
+  color: var(--color-letra-seleccion); /* Color del texto al pasar el mouse */
+}
+
 </style>
