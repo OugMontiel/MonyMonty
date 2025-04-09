cf6a7724a7a264e9e5beb5be93284399d1f07107
PcGamer <gogoedy@hotmail.com>
2025-04-09 17:31:28 -0500

    feat: :zap: Cambio de ubicacion de archivos
    Seguimos organizando para un desarrollo mas rapido

--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,45 @@
 import {createRouter, createWebHistory} from "vue-router";
-import dashboard from "../views/dashboard.vue";
-import document from "../views/document.vue";
-import NewMov from "../views/NewMov.vue";
-import home from "../views/home.vue";
+import dashboard from "../views/estaticas/dashboard.vue";
+import document from "../views/documentation.vue";
+import NewMov from "../views/movimientos/newMovemet.vue";
+import home from "../views/estaticas/home.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    // vistas estaticas inforamtivas
     {
       path: "/",
       name: "vista inciacial",
       component: home,
     },
-    {
-      path: "/dashboard",
-      name: "vista para todo el tablero ",
-      component: dashboard,
-    },
-    {
-      path: "/NewMov",
-      name: "Vista para nuevos movimiento",
-      component: NewMov,
-    },
+    // vista estaticas Documentacion
     {
       path: "/Document",
       name: "Vista de la Documentacion",
       component: document,
     },
+    // vistas de inicio
+    {
+      path: "/dashboard",
+      name: "vista para todo el tablero ",
+      component: dashboard,
+    },
+    // vistas de incio de seccion
+
+    // vistas de presupuesto
+
+    // vistas de seguimiento de presupuesto
+
+    // vistas de seguimiento de movimiento
+    {
+      path: "/NewMov",
+      name: "Vista para nuevos movimiento",
+      component: NewMov,
+    },
+    // vistas de reportes
+
+    // vistas de configuracion
   ],
 });
 
