1b02276c377a76d2bbec2adac1c062c8b993c55a
Hinoko <johameliasid@gmail.com>
2025-08-19 22:17:28 -0500

    la parte del login se separo

---
+++ src/features/userAcess/views/LoginView.vue
@@ -0,0 +1,390 @@
+<script>
+import api from "@/services/axios"; 
+
+import logo from "@/assets/img/MonyMontySinFondo3.png";
+
+import HeaderView from "@/features/auth/components/header.vue";
+import InfoView from "@/features/auth/components/infoLogin.vue";
+
+import CustomButton from "@/features/auth/components/CustomButton.vue";
+
+
+export default {
+  name: "Login",
+  components: {
+    HeaderView,
+    InfoView,
+    CustomButton,
+  },
+  data() {
+    return {
+      // variable del logo
+      logo,
+
+      // Variable para Mostrar error
+      showError: false,
+
+      // Datos para el inicio de sesión
+      username: "",
+      password: "",
+
+    };
+  },
+  methods: {
+    
+    /*async login() {
+      if (this.username && this.password) {
+        // Lógica para iniciar sesión
+        try {
+          const response = await api.post('http://localhost:3000/auth/login', {
+            email: this.username,
+            password: this.password
+          });
+
+          if (response.data.token) {
+            //localStorage.setItem('token', response.data.token);
+            // Redirige al Dashboard de la aplicación
+            //this.redirectToDashboard();
+          }else {
+            this.showErrorCuenta = true;
+          } 
+        } catch (error) {
+           this.showErrorCuenta = true;
+        }
+      } else {
+        this.showError = true;
+      }
+        
+    },
+    */
+    hideError() {
+      this.showError = false;
+    },
+    redirectToDashboard() {
+      // Lógica para redirigir al dashboard
+      this.$router.push("/tablero");
+    },
+    redirectToRecuperarCuenta() {
+      // Logica para redirigir a la interfaz de recuperar cuenta
+      this.$router.push("/recuperarCuenta");
+    },
+    redirectToCrearCuenta() {
+      // Lógica para redirigir a la interfaz de crear cuenta
+      this.$router.push("/crearCuenta");
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="login">
+    <HeaderView />
+    <div class="login-container">
+      <!-- Sección derecha -->
+      <div class="login-card">
+        <div class="login-form">
+          <div>
+            <img :src="logo" alt="Icono de la aplicación" class="login-logo" />
+          </div>
+
+          <div class="login-form-datos">
+            <div class="input-group">
+              <input type="email" v-model="username" placeholder=" " id="email" class="login-input"
+                @focus="hideError" />
+              <label for="email">Correo electrónico</label>
+            </div>
+
+            <div class="input-group">
+              <input type="password" v-model="password" placeholder=" " id="password" class="login-input"
+                @focus="hideError" />
+              <label for="password">Contraseña</label>
+            </div>
+
+            <p v-if="showError" class="login-error">Por favor, completa todos los campos.</p>0
+            <p v-if="showErrorCuenta" class="login-error">Error al iniciar sesión. Verifica tus datos.</p>
+          </div>
+
+          <CustomButton label="Iniciar Sesion" :customClick="login" background="var(--color-fondo-button-blue)"
+            text-color="var(--texto-primario-Blanco)" />
+
+          <p @click="redirectToRecuperarCuenta" class="forgot-password">¿Olvidaste tu contraseña?</p>
+
+          <div class="divider-buton"></div>
+
+          <CustomButton label="Crear cuenta nueva" :to="'/crearCuenta'" background="var(--color-fondo-button-green)"
+            text-color="var(--texto-primario-Blanco)" />
+        </div>
+      </div>
+
+      <div class="divider"></div>
+
+      <!-- Sección izquierda -->
+      <InfoView />
+    </div>
+
+    <!-- mensaje de pie de pagina -->
+    <div class="login-mesaje">
+      <p>"El dinero es un terrible amo pero un excelente sirviente." - P.T. Barnum</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+/* estilo general */
+
+.login {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  background-color: var(--color-fondo-login);
+  height: 100vh;
+  font-family: sans-serif;
+}
+
+.login-container {
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  width: 65%;
+  height: 60%;
+  margin: auto;
+  background: var(--color-fondo-login);
+  border-radius: 0.5rem;
+  box-shadow: 0 8px 30px var(--color-fondo-shadow);
+}
+
+/* Sección derecha */
+
+.login-logo {
+  max-width: 9rem;
+}
+
+.login-card {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 90%;
+  width: 30%;
+  padding: 0.2rem;
+  border-radius: 0.2rem;
+  background: var(--color-fondo-login);
+}
+
+.login-form {
+  height: 90%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.login-form-datos {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+}
+
+.input-group {
+  position: relative;
+  margin: 1rem 0;
+}
+
+.input-group input {
+  width: 100%;
+  padding: 1rem 0.5rem 0.5rem 0.5rem;
+  font-size: 1rem;
+  border: 1px solid var(--color-border-input);
+  border-radius: 5px;
+  outline: none;
+}
+
+.input-group label {
+  position: absolute;
+  top: 0.8rem;
+  left: 0.5rem;
+  color: var(--texto-primario);
+  font-size: 1rem;
+  pointer-events: none;
+  transition: 0.2s ease all;
+  padding: 0 0.25rem;
+}
+
+.input-group input:focus+label,
+.input-group input:not(:placeholder-shown)+label {
+  top: -0.5rem;
+  left: 0.4rem;
+  font-size: 0.75rem;
+  color: var(--texto-primario);
+  background: var(--color-fondo-login);
+}
+
+.login-error {
+  color: red;
+  font-size: 0.9rem;
+  margin-bottom: 1rem;
+}
+
+.login-button {
+  width: 100%;
+  background: var(--color-fondo-button);
+  color: var(--texto-primario-Blanco);
+  border: none;
+  padding: 0.8rem;
+  margin-bottom: 1rem;
+  font-weight: bold;
+  font-size: var(--font-size-botones);
+  border-radius: var(--border-radius-botones);
+  cursor: pointer;
+}
+
+.login-button:hover {
+  background: var(--color-fondo-button-activo);
+}
+
+.forgot-password {
+  color: var(--texto-primario-azul);
+  font-size: var(--font-size-botones);
+  cursor: pointer;
+  margin-bottom: 10px;
+}
+
+.forgot-password:hover {
+  text-decoration: none;
+  font-weight: bold;
+  color: var(--texto-primario-azul-hover);
+}
+
+.create-button {
+  width: 100%;
+  background-color: #28a745;
+  color: var(--texto-primario-Blanco);
+  border: none;
+  padding: 10px;
+  font-weight: bold;
+  font-size: var(--font-size-botones);
+  border-radius: var(--border-radius-botones);
+  cursor: pointer;
+}
+
+.create-button:hover {
+  background-color: #36a420;
+}
+
+
+.login-mesaje {
+  text-align: center;
+  width: 100%;
+  padding: 10px 0;
+  font-size: 0.9em;
+  color: #6c757d;
+}
+
+
+/* Extra pequeño: móviles pequeños (xs) */
+@media (max-width: 575.98px) {
+  .login-container {
+    flex-direction: column;
+    width: 90%;
+    height: auto;
+    margin-top: 6rem;
+    margin-bottom: 30px;
+  }
+
+  .login-card,
+  .login-info {
+    width: 100%;
+    height: auto;
+    padding: 10px;
+  }
+
+  .login-form {
+    width: 100%;
+  }
+
+  .login-form input {
+    font-size: 14px;
+  }
+
+  .login-button,
+  .create-button {
+    font-size: 14px;
+    padding: 8px;
+  }
+
+  .divider-buton {
+    width: 50%;
+    height: 5px;
+    margin: 15px 0;
+    background-color: #e0e0e0;
+  }
+
+  .divider {
+    width: 100%;
+    height: 2px;
+    margin: 15px 0;
+    background-color: #e0e0e0;
+  }
+
+  .login-info section {
+    text-align: center;
+    margin-bottom: 1rem;
+  }
+
+  .login-info h2,
+  .info-p,
+  .info-li {
+    font-size: 12px;
+  }
+
+  .login-logo {
+    max-width: 120px;
+  }
+
+  .login-footer {
+    font-size: 12px;
+    margin: auto;
+    padding: 5px;
+  }
+}
+
+/* Pequeño: móviles medianos y grandes (sm) */
+@media (min-width: 576px) and (max-width: 767.98px) {}
+
+/* Mediano: tablets (md) */
+@media (min-width: 768px) and (max-width: 991.98px) {}
+
+/* Grande: laptops (lg) */
+@media (min-width: 992px) and (max-width: 1199.98px) {
+  .login-container {
+    width: 85%;
+    height: 60%;
+  }
+
+  .login-card {
+    margin: 1rem;
+    /* Más espacio alrededor */
+    width: 45%;
+    /* Un poco más ancho */
+    padding: 1rem;
+    /* Interior más aireado */
+  }
+
+  .login-form {
+    max-width: 100%;
+  }
+
+  .login-logo {
+    max-width: 12rem;
+    /* Logo más grande */
+  }
+}
+
+/* Extra grande: pantallas grandes (xl) */
+@media (min-width: 1200px) and (max-width: 1399.98px) {}
+
+/* XXL: monitores muy grandes */
+@media (min-width: 1400px) {}
+</style>
---
+++ src/features/userAcess/views/RecuperarCuentaView.vue
@@ -0,0 +1,218 @@
+<script>
+import logo from "@/assets/img/MonyMontySinFondo3.png";
+import Footer from "@/components/web/footer.vue";
+
+export default {
+  name: "RecuperarCuenta",
+  data() {
+    return {
+      // variable del logo
+      logo,
+    };
+  },
+  components: {Footer},
+  methods: {
+    cancelar() {
+      this.$router.push("/");
+    },
+    buscarCuenta() {
+      this.$router.push("/");
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="logo-section">
+    <img :src="logo" alt="Icono de la aplicación" class="logo-icon" />
+  </div>
+
+  <div class="form-section">
+    <div class="card">
+      <h2 class="text-h2">Recupera tu cuenta</h2>
+      <p class="text-p">Ingresa tu correo electrónico para buscar tu cuenta.</p>
+
+      <input type="email" placeholder="Correo electrónico" class="input-text" />
+
+      <div class="divider"></div>
+
+      <div class="acciones">
+        <button class="btn-cancelar" @click="cancelar">Cancelar</button>
+        <button class="btn-buscar" @click="buscarCuenta">Buscar</button>
+      </div>
+    </div>
+
+    <!-- mensaje de pie de pagina -->
+    <div class="form-footer">
+      <p class="footer-p">"El dinero es un terrible amo pero un excelente sirviente." - P.T. Barnum</p>
+    </div>
+
+    <Footer />
+  </div>
+</template>
+
+<style scoped>
+
+/* Estructura general */
+.logo-section {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem;
+  background: var(--color-fondo-nav);
+}
+
+.logo-icon {
+  width: 8rem;
+  height: auto;
+}
+
+.form-section {
+  padding: 1rem;
+  background: var(--color-fondo);
+}
+
+/* Card principal */
+.card {
+  display: flex;
+  flex-direction: column;
+  max-width: 30%;
+  height: 80%;
+  margin: 7rem auto;
+  background: var(--color-fondo-login);
+  padding: 1.5em;
+  border: 1px solid var(--color-border);
+  border-radius: var(--border-radius-card);
+  box-shadow: 0 0.5rem 2rem var(--color-fondo-shadow);
+}
+
+.text-h2 {
+  font-size: var(--font-size-titulo);
+  margin-bottom: 0.5em;
+}
+
+.text-p {
+  font-size: var(--font-size-text);
+  color: var(--texto-segundario);
+  margin-bottom: 1em;
+}
+
+.input-text {
+  width: 100%;
+  padding: 0.7em;
+  border: 2px solid var(--color-border);
+  border-radius: var(--border-radius-botones);
+  font-size: var(--font-size-input);
+}
+
+.input-text:focus {
+  outline: none;
+  border: 2px solid var(--color-fondo-input-activo);
+}
+
+
+.divider {
+  width: 100%;
+  height: 0.1rem;
+  background: var(--color-fondo-linea);
+  margin: 1rem 0;
+}
+
+.acciones {
+  display: flex;
+  justify-content: flex-end;
+  gap: 0.5em;
+  margin: 1rem 0;
+}
+
+.btn-cancelar {
+  background: var(--color-fondo-input);
+  color: var(--texto-primario);
+  border: none;
+  padding: 10px 30px;
+  border-radius: var(--border-radius-botones);
+  border: 1px solid var(--color-border);
+  font-weight: bold;
+  font-size: var(--font-size-botones);
+  cursor: pointer;
+}
+
+.btn-cancelar:hover {
+  background: var(--color-fondo-input-activo);
+}
+
+.btn-buscar {
+  background: var(--color-fondo-button);
+  color: var(--texto-primario-Blanco);
+  border: none;
+  padding: 0.8rem 2rem;
+  border-radius: var(--border-radius-botones);
+  border: 1px solid var(--color-fondo-button);
+  font-weight: bold;
+  font-size: var(--font-size-botones);
+  cursor: pointer;
+}
+
+.btn-buscar:hover {
+  background: var(--color-fondo-button-activo);
+}
+
+.form-footer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 2rem;
+  text-align: center;
+}
+
+.footer-p {
+  font-style: var(--font-family-base);
+  font-size: var(--font-size-text);
+  color: var(--texto-segundario);
+}
+
+/* Extra pequeño: móviles pequeños (xs) */
+@media (max-width: 575.98px) {
+  .card {
+    margin: 30px auto;
+    padding: 1.5em;
+    border: 1px solid var(--color-border);
+    border-radius: 2px;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1);
+  }
+
+  .footer-p {
+    font-style: italic;
+    font-size: 15px;
+    color: #333;
+  }
+
+  .btn-cancelar {
+    padding: 10px 15px;
+  }
+
+  .btn-buscar {
+    padding: 10px 20px;
+  }
+}
+
+/* Pequeño: móviles medianos y grandes (sm) */
+@media (min-width: 576px) and (max-width: 767.98px) {
+}
+
+/* Mediano: tablets (md) */
+@media (min-width: 768px) and (max-width: 991.98px) {
+}
+
+/* Grande: laptops (lg) */
+@media (min-width: 992px) and (max-width: 1199.98px) {
+}
+
+/* Extra grande: pantallas grandes (xl) */
+@media (min-width: 1200px) and (max-width: 1399.98px) {
+}
+
+/* XXL: monitores muy grandes */
+@media (min-width: 1400px) {
+}
+</style>
