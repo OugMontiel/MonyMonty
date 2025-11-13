f89cd43800a3c938893af111fa4604bf3c1b0da4
PcGamer <gogoedy@hotmail.com>
2025-11-12 20:08:42 -0500

    Ajuste tablero

--- src/features/dashBoard/views/elTablero.vue
+++ src/features/dashBoard/views/elTablero.vue
@@ -1,8 +1,8 @@
 <script>
-import HeaderView from "@/features/dashBoard/components/tablero/header.vue";
-import SidebarView from "@/features/dashBoard/components/tablero/sidebar.vue";
-import FooterView from "@/features/dashBoard/components/tablero/footer.vue";
-import BackgroundView from "@/features/dashBoard/components/dashBoardMovimiento/dashboardView.vue";
+import HeaderView from "../components/tablero/header.vue";
+import SidebarView from "../components/tablero/sidebar.vue";
+import FooterView from "../components/tablero/footer.vue";
+import BackgroundView from "../components/dashBoardMovimiento/dashboardView.vue";
 
 export default {
   name: "ElTablero",
@@ -14,28 +14,21 @@ export default {
   },
 };
 </script>
-
 <template>
-  <div class="flex flex-col h-screen">
+  <div class="min-h-screen flex flex-col">
     <!-- Header -->
     <HeaderView />
 
-    <!-- Main Layout: Sidebar + Content -->
-    <div class="flex flex-1 min-h-0">
+    <div class="flex flex-1">
       <!-- Sidebar -->
       <SidebarView />
 
-      <!-- Main Content Area -->
-      <div class="flex-1 overflow-auto bg-gray-50">
+      <!-- Main Content / Background -->
+      <main class="flex-1 relative">
         <BackgroundView />
-      </div>
+      </main>
     </div>
-
-    <!-- Footer -->
-    <FooterView />
   </div>
 </template>
 
-<style scoped>
-/* Solo estilos que no se pueden lograr con Tailwind o personalizaciones específicas */
-</style>
+<style scoped></style>
