a46a6757d62804305da85115c3476711f958b944
PcGamer <gogoedy@hotmail.com>
2026-09-11 17:33:58 -0500

    fix: update path import and alias configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import {defineConfig} from "vite";
 import vue from "@vitejs/plugin-vue";
-import path from "path";
+import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"), // Configuración de alias
+      "@": path.resolve(import.meta.dirname, "src"), // Configuración de alias actualizada
     },
   },
   server: {
